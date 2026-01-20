b6b8932b5b7b2ed4e55267c941478ae1bf9be048
Michaela Zachardalová <michaela.zachardalova@gmail.com>
2026-01-20 04:19:39 +0100

    Můj první program

---
+++ IS-Programy/program001-vypis-rady/Program.cs
@@ -0,0 +1,25 @@
+﻿// Zeptáme se uživatele na první číslo
+Console.WriteLine("Zadejte první hodnotu (first):");
+string vstupFirst = Console.ReadLine();
+int first = int.Parse(vstupFirst); // Převedeme text na číslo
+
+// Zeptáme se na poslední číslo
+Console.WriteLine("Zadejte poslední hodnotu (last):");
+string vstupLast = Console.ReadLine();
+int last = int.Parse(vstupLast);
+
+// Zeptáme se na krok
+Console.WriteLine("Zadejte krok (step):");
+string vstupStep = Console.ReadLine();
+int step = int.Parse(vstupStep);
+
+Console.WriteLine("--- Výpis řady ---");
+
+// Tady je smyčka (cyklus). 
+// Začneme na čísle 'first'.
+// Dokud je číslo menší nebo rovno 'last', pokračujeme.
+// Po každém kole přičteme 'step'.
+for (int i = first; i <= last; i = i + step)
+{
+    Console.WriteLine(i);
+}
